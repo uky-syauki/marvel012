b6f52bcf544155fa44b7ab9faae76b751b683d8a
uky-syauki <achmad.uky@gmail.com>
2023-10-20 02:40:34 +0800

    3 local

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, jsonify
 
 from app import app
 
@@ -6,3 +6,9 @@ from app import app
 @app.route('/index')
 def index():
 	return render_template('index.html')
+
+
+@app.route('/api/getData', methods=['GET'])
+def getData():
+	data = {'nama':"Ahmad Syauki"}
+	return jsonify(data)
