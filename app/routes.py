406bde5661e836ca93fc25e460bf7681cff0a138
uky-syauki <achmad.uky@gmail.com>
2023-10-22 07:28:42 +0800

    5 local

--- app/routes.py
+++ app/routes.py
@@ -15,10 +15,22 @@ def getData():
 	return jsonify(data)
 
 
-@app.route('/api/postData', methods=['POST','GET'])
+@app.route('/api/postData', methods=['POST'])
 def postData():
 	nama_lengkap = request.form.get('nama_lengkap')
-	print(nama_lengkap)
+	add_calgot = calgot(
+		nama_lengkap=request.form.get('nama_lengkap'),
+		nama_panggilan=request.form.get('nama_panggilan'),
+		jenis_kelamin=request.form.get('jenis_kelamin'),
+		email=request.form.get('email'),
+		nomor_wa=request.form.get('nomor_wa'),
+		alamat=request.form.get('alamat'),
+		kampus=request.form.get('kampus'),
+		alasan=request.form.get('alasan')
+	)
+	db.session.add(add_calgot)
+	db.session.commit()
+	# print(nama_lengkap)
 	# data = request.get_json()
 	# respon = {'pesan':f'Data Telah diterima, data:{data}'}
 	# print(data)
